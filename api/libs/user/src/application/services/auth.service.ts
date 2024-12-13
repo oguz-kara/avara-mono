b767e681852e68c9a238274179ceb56c9392ad55
Hasan Kara <dev.hasan.kara@gmail.com>
2024-12-13 17:56:39 +0300

    fix: set cookie secure false

--- api/libs/user/src/application/services/auth.service.ts
+++ api/libs/user/src/application/services/auth.service.ts
@@ -104,7 +104,7 @@ export class AuthService {
     const token = this.signToken(user.id, user.email)
     context.req?.res?.cookie('access_token', token, {
       httpOnly: true,
-      secure: true,
+      secure: false,
       sameSite: 'strict',
       maxAge: 30 * 24 * 60 * 60 * 1000,
     })
